541fae33c8ad9658f1e114b87d80472b4f1364e9
MrKleshch <130285407+MrKleshch@users.noreply.github.com>
2024-08-24 15:26:00 +0600

    two

--- testCpp/testCpp/testCpp.cpp
+++ testCpp/testCpp/testCpp.cpp
@@ -4,34 +4,26 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    vector<int> shirts(n);
-    for (int i = 0; i < n; ++i) cin >> shirts[i];
+    vector<int> N(n);
+    for (int i = 0; i < n; ++i) cin >> N[i];
 
     int m; cin >> m;
-    vector<int> trousers(m);
-    for (int i = 0; i < m; ++i) cin >> trousers[i];
+    vector<int> M(m);
+    for (int i = 0; i < m; ++i) cin >> M[i];
 
     int i = 0, j = 0;
     int nbest = 0, mbest = 0;
 
     while (i < n && j < m) {
-        if (abs(shirts[i] - trousers[j]) < abs(shirts[nbest] - trousers[mbest])) {
+        if (abs(N[i] - M[j]) < abs(N[nbest] - M[mbest])) {
             nbest = i;
             mbest = j;
         }
-
-        if (shirts[i] > trousers[j]) {
-            ++j;
-        }
-        else if (shirts[i] == trousers[j]) {
-            break;
-        }
-        else {
-            ++i;
-        }
+        if (N[i] > M[j]) ++j;
+        else if (N[i] < M[j]) ++i;
+        else break;
     }
 
-    cout << shirts[nbest] << " " << trousers[mbest] << std::endl;
-
+    cout << N[nbest] << " " << M[mbest];
     return 0;
 }
