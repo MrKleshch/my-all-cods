a3f2ead6b7c366fe8a668b44768d488149354b2e
mrkleshch <dimakl08@mail.ru>
2024-08-24 14:12:52 +0600

    first
    лололо

---
+++ testCpp/testCpp/testCpp.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int> shirts(n);
+    for (int i = 0; i < n; ++i) cin >> shirts[i];
+
+    int m; cin >> m;
+    vector<int> trousers(m);
+    for (int i = 0; i < m; ++i) cin >> trousers[i];
+
+    int i = 0, j = 0;
+    int nbest = 0, mbest = 0;
+
+    while (i < n && j < m) {
+        if (abs(shirts[i] - trousers[j]) < abs(shirts[nbest] - trousers[mbest])) {
+            nbest = i;
+            mbest = j;
+        }
+
+        if (shirts[i] > trousers[j]) {
+            ++j;
+        }
+        else if (shirts[i] == trousers[j]) {
+            break;
+        }
+        else {
+            ++i;
+        }
+    }
+
+    cout << shirts[nbest] << " " << trousers[mbest] << std::endl;
+
+    return 0;
+}
