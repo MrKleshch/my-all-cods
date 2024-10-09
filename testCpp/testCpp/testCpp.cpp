a3f7cafd4ae7a0e67694d9de3fe310f513660819
mrkleshch <dimakl08@mail.ru>
2024-10-09 22:13:33 +0600

    /

--- testCpp/testCpp/testCpp.cpp
+++ testCpp/testCpp/testCpp.cpp
@@ -1,43 +1,58 @@
 ﻿#include <iostream>
 #include <vector>
 #include <fstream>
-
 using namespace std;
 
-void dfs(int v, vector<vector<int>>& graph, vector<int>& visited, int color) {
-    visited[v] = color;
+vector<int> getCycle(int v, vector<int>& from) {
+    vector<int> ans = { v };
+    for (int i = from[v]; i != v; i = from[i]) ans.push_back(i);
+    return ans;
+}
+
+void dfs(vector<vector<int>>& graph, vector<int>& visited, int v,
+    vector<int>& from, vector<int>& cycle) {
+    visited[v] = 1;
     for (int to : graph[v]) {
         if (visited[to] == 0) {
-            dfs(to, graph, visited, color);
+            from[to] = v;
+            dfs(graph, visited, to, from, cycle);
+            if (!cycle.empty()) return;
+        }
+        else if (visited[to] == 1) {
+            from[to] = v;
+            cycle = getCycle(to, from);
+            return;
         }
     }
+    visited[v] = 2;
 }
 
 int main() {
-    ifstream in;  // для чтения данных
-    ofstream out; // для записи данных
+    ifstream in;
+    ofstream out;
     in.open("input.txt");
     out.open("output.txt");
 
-    int vertex, edge; in >> vertex >> edge;
-    vector<vector<int>> graph(vertex);
-    vector<int> visited(vertex, 0);
-    for (int i = 0; i < edge; ++i) {
+    int V, E; in >> V >> E;
+    vector<vector<int>> graph(V);
+    for (int i = 0; i < E; i++) {
         int a, b; in >> a >> b;
         graph[--a].push_back(--b);
         graph[b].push_back(a);
     }
 
-    int color = 1;
-    for (int i = 0; i < vertex; ++i) {
+    vector<int> visited(V, 0);
+    vector<int> from(V, -1);
+    vector<int> cycle;
+    for (int i = 0; i < V && cycle.empty(); i++) {
         if (visited[i] == 0) {
-            dfs(i, graph, visited, color++);
+            dfs(graph, visited, i, from, cycle);
         }
     }
-
-    for (int i : visited) out << i << " ";
-
-    in.close();
-    out.close();
-
+    /*for (int i = 0; i < V; i++) {
+        out << i + 1 << " - " << visited[i] << "\n";
+    }*/
+    for (int i : cycle) {
+        out << i + 1 << " ";
+    }
 }
