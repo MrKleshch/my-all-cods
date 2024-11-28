39bb33f2be473a0cd9bb66907c19e5cdd7d5e1c4
MrKleshch <130285407+MrKleshch@users.noreply.github.com>
2024-11-28 20:19:50 +0600

    2

--- testCpp/testCpp/testCpp.cpp
+++ testCpp/testCpp/testCpp.cpp
@@ -2,11 +2,19 @@
 #include <fstream>
 using namespace std;
 
+int a[1000010];
+
 int main() {
-    ifstream сin;
-    ofstream сout;
-    in.open("input.txt");
-    out.open("output.txt");
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int n; cin >> n;
+    unsigned long long a = 0;
+    for (int i = 0; i <= n; i++) a[i] = 0;
 
     
+
+    return 0;
 }
